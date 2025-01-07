5f4145981a48384ea6b4e63fd83f33dd247f62e6
Reshma13-design <reshmashinde1384@outlook.com>
2025-01-07 17:10:34 +0530

    Hello World

--- src/JavaPrograms/HelloWorld.java
+++ src/JavaPrograms/HelloWorld.java
@@ -3,6 +3,8 @@ package JavaPrograms;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hi, Reshma");
+        System.out.print("Hi, Reshma");
+
 
 
 
