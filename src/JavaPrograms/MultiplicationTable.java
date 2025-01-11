d140bcd69a7e5853b82c8221719a21e89d777d8c
Reshma13-design <reshmashinde1384@outlook.com>
2025-01-11 16:08:00 +0530

    Multiplication Table of 9

---
+++ src/JavaPrograms/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package JavaPrograms;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.printf("%d x %d = %d%n", 9, 1, 9 * 1);
+        System.out.printf("%d x %d = %d%n", 9, 2, 9 * 2);
+        System.out.printf("%d x %d = %d%n", 9, 3, 9 * 3);
+        System.out.printf("%d x %d = %d%n", 9, 4, 9 * 4);
+        System.out.printf("%d x %d = %d%n", 9, 5, 9 * 5);
+        System.out.printf("%d x %d = %d%n", 9, 6, 9 * 6);
+        System.out.printf("%d x %d = %d%n", 9, 7, 9 * 7);
+        System.out.printf("%d x %d = %d%n", 9, 8, 9 * 8);
+        System.out.printf("%d x %d = %d%n", 9, 9, 9 * 9);
+        System.out.printf("%d x %d = %d%n", 9, 10, 9 * 10);
+    }
+}
